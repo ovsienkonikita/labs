3443b7e5f875ee04332756c1424a24e74708f910
Ovsiienko Mykyta <ovsienkonikitav@gmail.com>
2019-11-10 00:41:24 +0200

    Adjusted code style, travis file changes

--- test/lab1/ListAnalyzerTest.java
+++ test/lab1/ListAnalyzerTest.java
@@ -11,7 +11,7 @@ class ListAnalyzerTest {
 
     @Test
     public void testSum() {
-        List<List<Double>> matrix = __generateMatrix();
+        List<List<Double>> matrix = generateMatrix();
         ListAnalyzer analyzer = new ListAnalyzer(matrix);
         double sum = analyzer.getSum();
         assertEquals(36100.0, sum);
@@ -19,7 +19,7 @@ class ListAnalyzerTest {
 
     @Test
     public void testMin() {
-        List<List<Double>> matrix = __generateMatrix();
+        List<List<Double>> matrix = generateMatrix();
         ListAnalyzer analyzer = new ListAnalyzer(matrix);
         double min = analyzer.getMin();
         assertEquals(0.0, min);
@@ -27,13 +27,13 @@ class ListAnalyzerTest {
 
     @Test
     public void testMax() {
-        List<List<Double>> matrix = __generateMatrix();
+        List<List<Double>> matrix = generateMatrix();
         ListAnalyzer analyzer = new ListAnalyzer(matrix);
         double max = analyzer.getMax();
         assertEquals(361.0, max);
     }
 
-    private List<List<Double>> __generateMatrix() {
+    private List<List<Double>> generateMatrix() {
         List<List<Double>> main_list = new ArrayList<>(20);
         for (int i = 0; i < 20; i++) {
             List<Double> child_list = new ArrayList<>(20);
